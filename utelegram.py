5cf7dc2c400b83466c790cc60c6e561534f0cf27
Marcello Sgarbi <marcello.sgr@gmail.com>
2023-10-08 13:25:58 +0200

    Removed not used files during the project implementation

--- utelegram.py
+++ utelegram.py
@@ -1,28 +1,24 @@
+"""
+This code will be executed on Raspberry Pi Pico W.
+During tests with Firmware micropython-firmware-pico-w-130623.uf2
+    it was NOT possible to get a response from the REST request
+    using threading.
+    Because of this the suggestion is to use it on the main
+    thread in which it worked perfectly.
+"""
+
+
 import time
 import gc
-import ujson
 import urequests
 
-
-class ubot:
-    
+class Ubot:
     def __init__(self, token, offset=0):
         self.url = 'https://api.telegram.org/bot' + token
         self.commands = {}
         self.default_handler = None
         self.message_offset = offset
         self.sleep_btw_updates = 3
-
-        messages = self.read_messages()
-        if messages:
-            if self.message_offset==0:
-                self.message_offset = messages[-1]['update_id']
-            else:
-                for message in messages:
-                    if message['update_id'] >= self.message_offset:
-                        self.message_offset = message['update_id']
-                        break
-
 
     def send(self, chat_id, text):
         data = {'chat_id': chat_id, 'text': text}
@@ -31,7 +27,7 @@ class ubot:
             response = urequests.post(self.url + '/sendMessage', json=data, headers=headers)
             response.close()
             return True
-        except:
+        except Exception:
             return False
 
     def read_messages(self):
@@ -43,7 +39,10 @@ class ubot:
             'allowed_updates': ['message']}
 
         try:
-            update_messages = urequests.post(self.url + '/getUpdates', json=self.query_updates).json() 
+            # print("Getting messages...")
+            # print(f"Link: {self.url + '/getUpdates'}")
+            update_messages = urequests.post(self.url + '/getUpdates', json=self.query_updates).json()
+            print(update_messages)  # Debug printing
             if 'result' in update_messages:
                 for item in update_messages['result']:
                     result.append(item)
@@ -61,9 +60,10 @@ class ubot:
             gc.collect()
 
     def read_once(self):
+        gc.collect()
         messages = self.read_messages()
         if messages:
-            if self.message_offset==0:
+            if self.message_offset == 0:
                 self.message_offset = messages[-1]['update_id']
                 self.message_handler(messages[-1])
             else:
@@ -72,7 +72,7 @@ class ubot:
                         self.message_offset = message['update_id']
                         self.message_handler(message)
                         break
-    
+
     def register(self, command, handler):
         self.commands[command] = handler
 
